94472084f271510b6cf582a218d264d26d356b77
Andrew Heimbuch <andrewh@andrewheimbuch.com>
2012-10-08 13:26:49 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+	<meta charset=utf-8>
+	<title></title>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<link rel="stylesheet" href="Style.css" type="text/css" />
+</head>
+<body>
+</body>
+</html>
